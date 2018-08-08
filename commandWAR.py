ed3cbae1992c6f175c8c5871a635b935601179d5
root <Napoleon3500@gmail.com>
2018-08-08 09:24:10 -0700

    New stuff coming

--- commandWAR.py
+++ commandWAR.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
-import requests
+import praw
 
 
 def war(sub):
 
-    
+    user = 'USS_Bot'
+    r = praw.Reddit('bot' , user_agent=user)
+
+
     
     
 war('worldofwarships')
